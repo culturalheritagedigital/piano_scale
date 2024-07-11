113dbadad804a5ce5f4eef47517170fc3ae79119
Niko Plath <38769224+culturalheritagedigital@users.noreply.github.com>
2024-07-11 10:05:10 +0200

    dd

--- piano_scale.py
+++ piano_scale.py
@@ -6,6 +6,14 @@ st.title('Piano Scale Calculation')
 
 st.write('This tool is supposed to help you calculate the parameters of a piano string scale.')
 
+st.checkbox('Check me out')
+
+st.write('Here\'s our first attempt at using data to create a table:')
+
+dataframe = {
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}
 
 
 dataframe = np.random.randn(30, 30)
