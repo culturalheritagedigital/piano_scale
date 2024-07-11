39f4764e19cbecd95a41051bfe249de33bcf364a
Niko Plath <38769224+culturalheritagedigital@users.noreply.github.com>
2024-07-11 10:17:44 +0200

    s

--- piano_scale.py
+++ piano_scale.py
@@ -5,13 +5,14 @@ from scipy.io import wavfile
 
 st.title('Piano Scale Calculation')
 
-st.write('This tool is supposed to help you calculate the parameters of a piano string scale.')
+#st.write('This tool is supposed to help you calculate the parameters of a piano string scale.')
 
-st.checkbox('Check me out')
+#st.checkbox('Check me out')
 
 # st.write('Here\'s our first attempt at using data to create a table:')
 # dataframe = np.random.randn(30, 30)
 # st.dataframe(dataframe)
 
-kammerton = st.number_input("Insert concert pitch")
+kammerton = st.number_input("Insert concert pitch", value=440.0)
 st.write("The current concert pitch is ", kammerton, " Hz.")
+
