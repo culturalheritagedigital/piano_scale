35dfaa37c2feb0b8d99f22222d354099ebf14601
Niko Plath <38769224+culturalheritagedigital@users.noreply.github.com>
2024-07-11 17:04:42 +0200

    Update piano_scale.py

--- piano_scale.py
+++ piano_scale.py
@@ -61,7 +61,7 @@ st.title('Piano Scale Calculation')
 # dataframe = np.random.randn(30, 30)
 # st.dataframe(dataframe)
 
-kammerton = st.number_input("Insert concert pitch:", value=440, step=1)
+kammerton = st.number_input("Choose a concert pitch:", value=440, step=1)
 st.write("The current concert pitch is ", kammerton, " Hz.")
 
 def f(key):
