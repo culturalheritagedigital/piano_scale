6cd455eb42579d2e5bbc8ffca1e8f8bcb31f89d1
Niko Plath <38769224+culturalheritagedigital@users.noreply.github.com>
2024-07-11 10:49:02 +0200

    Update piano_scale.py

--- piano_scale.py
+++ piano_scale.py
@@ -87,4 +87,4 @@ four = np.abs(np.fft.fft(signal[0:48000]))
 four = four/np.max(four)
 fourlog = 20*np.log10(four/np.max(four))
 
-st.line_chart(fourlog[0:f(key_num)*(n+2)])
+st.line_chart(fourlog[0:int(f(key_num)*(n+2))])
