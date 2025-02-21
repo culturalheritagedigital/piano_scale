26a93f9db16990c627b0c5d4f3f401af151cf868
Niko Plath <38769224+culturalheritagedigital@users.noreply.github.com>
2025-02-21 11:00:55 +0100

    Update 2_Schwebungen.py

--- pages/2_Schwebungen.py
+++ pages/2_Schwebungen.py
@@ -172,9 +172,9 @@ st.write("Wählen Sie zwei Frequenzen aus:")
 
 f1 = st.number_input("f1:", value=440, min_value=1, max_value=4400)
 
-f2 = st.number_input("f2:", value=440, min_value=1, max_value=4400)
+f2 = st.number_input("f2:", value=f1+1, min_value=1, max_value=4400)
 
-f2 = st.slider("f2:", min_value=f2, max_value=f1*2, value=f2, step=1)
+#f2 = st.slider("f2:", min_value=f2, max_value=f1*2, value=f2, step=1)
 
 st.write("Frequenzdifferenz: " + str(f2-f1) + " Hz.")
 
